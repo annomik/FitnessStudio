beeb7b0baf013c24d38df98d66e33a6c1a1cf567
Hanna Mikulich <mikulichhanna@gmail.com>
2023-09-11 22:58:11 +0300

    Added annotations to Dto classes

--- user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/core/dto/user/UserCreateDTO.java
+++ user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/core/dto/user/UserCreateDTO.java
@@ -2,6 +2,10 @@ package by.it_academy.jd2.MJD29522.fitness.core.dto.user;
 
 import by.it_academy.jd2.MJD29522.fitness.enums.UserRole;
 import by.it_academy.jd2.MJD29522.fitness.enums.UserStatus;
+import by.it_academy.jd2.MJD29522.fitness.validator.api.ValidEmail;
+import by.it_academy.jd2.MJD29522.fitness.validator.api.ValidString;
+import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.NumberFormat;
 
 import java.time.LocalDateTime;
 import java.util.Objects;
@@ -11,13 +15,18 @@ import java.util.UUID;
 public class UserCreateDTO {
 
     private UUID uuid;
-
+    @NumberFormat
     private LocalDateTime dtCreate;
     private LocalDateTime dtUpdate;
+    @ValidEmail
     private String mail;
+    @ValidString
     private String fio;
+    @NotNull(message = "Valid values: USER, ADMIN")
     private UserRole role;
+    @ValidString(message = "Valid values: WAITING_ACTIVATION, ACTIVATED, DEACTIVATED")
     private UserStatus status;
+    @ValidString
     private String password;
 
     public UserCreateDTO() {
--- user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/core/dto/user/UserRegistrationDTO.java
+++ user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/core/dto/user/UserRegistrationDTO.java
@@ -1,11 +1,17 @@
 package by.it_academy.jd2.MJD29522.fitness.core.dto.user;
 
+import by.it_academy.jd2.MJD29522.fitness.validator.api.ValidEmail;
+import by.it_academy.jd2.MJD29522.fitness.validator.api.ValidString;
+
 import java.util.Objects;
 
 public class UserRegistrationDTO {
 
+    @ValidEmail
     private String mail;
+    @ValidString
     private String fio;
+    @ValidString
     private String password;
 
     public UserRegistrationDTO() {
