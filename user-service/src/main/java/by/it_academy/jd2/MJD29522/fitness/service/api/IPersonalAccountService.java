42a226dec33bad66efd6cadf5a3e592139d38e68
Hanna Mikulich <mikulichhanna@gmail.com>
2023-09-11 22:57:33 +0300

    Added annotations to interfaces

--- user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/service/api/IPersonalAccountService.java
+++ user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/service/api/IPersonalAccountService.java
@@ -4,21 +4,19 @@ package by.it_academy.jd2.MJD29522.fitness.service.api;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.user.UserDTO;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.user.UserLoginDTO;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.user.UserRegistrationDTO;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
+
 import java.util.UUID;
 
 public interface IPersonalAccountService {
 
-    boolean save(UserRegistrationDTO userRegistrationDTO);
+    boolean save(@NotNull @Valid UserRegistrationDTO userRegistrationDTO);
 
     UserDTO getCard(UUID id);
 
-   // UserDTO aboutMe(String mail);
-
     void verify(String verificationCode, String mail);
 
-    UserDTO login(UserLoginDTO userLoginDTO);
-
-    void validate(UserRegistrationDTO userRegistrationDTO);
-
+    UserDTO login(@NotNull @Valid UserLoginDTO userLoginDTO);
 
 }
--- user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/service/api/IUserService.java
+++ user-service/src/main/java/by/it_academy/jd2/MJD29522/fitness/service/api/IUserService.java
@@ -3,17 +3,20 @@ package by.it_academy.jd2.MJD29522.fitness.service.api;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.PageDTO;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.user.UserCreateDTO;
 import by.it_academy.jd2.MJD29522.fitness.core.dto.user.UserDTO;
+import by.it_academy.jd2.MJD29522.fitness.validator.api.ValidString;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
 public interface IUserService {
 
-    void addNewUser(UserCreateDTO userCreateDTO) ;
+    void addNewUser(@NotNull @Valid UserCreateDTO userCreateDTO) ;
 
     UserDTO getCard(UUID uuid);
 
-    void update(UUID uuid, LocalDateTime dtUpdate, UserCreateDTO userCreateDTO);
+    void update(@ValidString UUID uuid, @NotNull LocalDateTime dtUpdate, @NotNull @Valid UserCreateDTO userCreateDTO);
 
     UserDTO getUserByMail(String mail);
 
